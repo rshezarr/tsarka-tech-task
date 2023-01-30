8022dbd0680472ed7d3c3caad7d08461e19fd0b4
rshezarr <rshezarr@gmail.com>
2023-01-30 06:35:05 +0600

    init: app

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,21 @@
 package app
+
+import (
+	"log"
+	"net/http"
+	"rest-api/internal/handlers"
+)
+
+func Start() {
+	handler := handlers.NewHandler()
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: handler.InitRoutes(),
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Printf("error occured while starting server: %e", err)
+		return
+	}
+}
