94c95a37b21112df1773dd85488dfc9c131143b0
rshezarr <rshezarr@gmail.com>
2023-03-30 19:49:57 +0600

    init: routes

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -1,1 +1,22 @@
 package handlers
+
+import (
+	"net/http"
+)
+
+type Handler struct {
+	mux *http.ServeMux
+}
+
+func NewHandler() *Handler {
+	return &Handler{
+		mux: http.NewServeMux(),
+	}
+}
+
+func (h *Handler) InitRoutes() *http.ServeMux {
+	h.mux.HandleFunc("/rest/substr/find", nil)
+	h.mux.HandleFunc("/rest/email/check", nil)
+
+	return h.mux
+}
