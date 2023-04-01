77a30d08dccf48489c95274846ec04416eb56ea8
rshezarr <rshezarr@gmail.com>
2023-04-02 02:07:37 +0600

    add: logging middleware

--- internal/handlers/middleware.go
+++ internal/handlers/middleware.go
@@ -1,1 +1,15 @@
 package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func (h *Handler) loggingMiddleware(router http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("%s %s [%s]\t%s%s - 200 - OK", time.Now().Format("2006/01/02 15:04:05"), r.Proto, r.Method, r.Host, r.RequestURI)
+		router.ServeHTTP(w, r)
+		fmt.Println()
+	}
+}
