1530b41cb1c3475297aa7d4573dc5a324b71a32a
rshezarr <rshezarr@gmail.com>
2023-04-02 03:21:47 +0600

    fix: logging middleware newline

--- internal/handlers/middleware.go
+++ internal/handlers/middleware.go
@@ -8,8 +8,7 @@ import (
 
 func (h *Handler) loggingMiddleware(router http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("%s %s [%s]\t%s%s - 200 - OK", time.Now().Format("2006/01/02 15:04:05"), r.Proto, r.Method, r.Host, r.RequestURI)
+		fmt.Printf("%s %s [%s]\t%s%s - 200 - OK\n", time.Now().Format("2006/01/02 15:04:05"), r.Proto, r.Method, r.Host, r.RequestURI)
 		router.ServeHTTP(w, r)
-		fmt.Println()
 	}
 }
