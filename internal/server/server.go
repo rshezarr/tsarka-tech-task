74e46b2a92bad3f602083fc17e907e05a1d98382
rshezarr <rshezarr@gmail.com>
2023-04-02 02:07:57 +0600

    update: server methods and configs

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	"net/http"
 	"time"
 )
@@ -13,17 +14,31 @@ const (
 )
 
 type Server struct {
-	server *http.Server
+	Srv               *http.Server
+	ServerErrorNotify chan error
 }
 
 func NewServer(router *http.ServeMux) *Server {
 	return &Server{
-		server: &http.Server{
+		Srv: &http.Server{
 			Addr:           Addr,
 			Handler:        router,
 			MaxHeaderBytes: MaxHeaderBytes,
 			ReadTimeout:    ReadTimeout,
 			WriteTimeout:   WriteTimeout,
 		},
+		ServerErrorNotify: make(chan error, 1),
 	}
+}
+
+func (s *Server) Start() {
+	s.ServerErrorNotify <- s.Srv.ListenAndServe()
+}
+
+func (s *Server) ServerErrNotify() <-chan error {
+	return s.ServerErrorNotify
+}
+
+func (s *Server) Shutdown() error {
+	return s.Srv.Shutdown(context.Background())
 }
