375622a4302d38fa53c4a6a8a62ee18bc8ec8919
rshezarr <rshezarr@gmail.com>
2023-04-02 01:49:35 +0600

    add: server configs

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,29 @@
 package server
+
+import (
+	"net/http"
+	"time"
+)
+
+const (
+	Addr           = ":9090"
+	MaxHeaderBytes = 1 << 20
+	ReadTimeout    = 10 * time.Second
+	WriteTimeout   = 10 * time.Second
+)
+
+type Server struct {
+	server *http.Server
+}
+
+func NewServer(router *http.ServeMux) *Server {
+	return &Server{
+		server: &http.Server{
+			Addr:           Addr,
+			Handler:        router,
+			MaxHeaderBytes: MaxHeaderBytes,
+			ReadTimeout:    ReadTimeout,
+			WriteTimeout:   WriteTimeout,
+		},
+	}
+}
